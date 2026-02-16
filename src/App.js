b4c49112371a2d9b5529b644863d1d389dca72ec
DIY Assist <james.mcintyre4@gmail.com>
2026-02-16 15:17:13 -0500

    fix for day 1 and day 2 info

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to The DIY Assist</h1>
+        <p>Expert help via video chat for DIY projects</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
